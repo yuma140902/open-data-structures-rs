05d724a99620a34eba06ebd6d6776ca6471cb358
yuma140902 <yuma140902@gmail.com>
2022-02-16 14:27:02 +0900

    add `SSet`

--- src/traits.rs
+++ src/traits.rs
@@ -57,3 +57,15 @@ pub trait USet<T> {
     /// `v`と同じ要素を探す。見つかったらその要素を、見つからなければ`None`を返す
     fn find(&self, v: &T) -> Option<T>;
 }
+
+/// ソートされた要素の集合。SはsortedのS。
+pub trait SSet<T> {
+    /// 集合の要素数を返す
+    fn size(&self) -> usize;
+    /// `v`が集合に入っていなければ追加する。`v`が集合に追加されたら`true`、そうでなければ`false`を返す
+    fn add(&mut self, v: T) -> bool;
+    /// 集合から`v`と同じ要素を削除する。削除されたらその要素を、同じ要素が見つからなければ`None`を返す
+    fn remove(&mut self, v: &T) -> Option<T>;
+    /// `w` ≧ `v`を満たす最小の要素`w`を見つける。`w`が存在すれば`Some(w)`を返し、存在しなけれ`None`を返す
+    fn find(&self, v: &T) -> Option<T>;
+}
